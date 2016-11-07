e4298be1971277f5e9bb9066ea6e5713f60c6fbb
Zach Gillis <zgillis11@gmail.com>
2016-11-07 07:55:55 -0600

    Just a quick review I did of my past C# knowledge. More to come.

---
+++ samplification/samplification/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharpPractice
+{
+	class Program
+	{
+		//declaring variables
+		static int current_year;
+		static int user_age;
+		static String input_age;
+
+		static void Main(string[] args)
+		{
+			//Finds the current ye
+			current_year = DateTime.Now.Year;
+
+			//Displays welcome
+			Console.WriteLine("Hello World! It's currently " + current_year + "!");
+			Thread.Sleep(2000);
+
+			Console.Write("How old are you? ");
+			input_age = Console.ReadLine();
+
+
+			if (int.TryParse(input_age, out user_age))
+			{
+				Console.WriteLine("Wow! You're becoming ancient if you're " + user_age + "already!");
+			}
+			else
+			{
+				Console.WriteLine("Lame-ass, that's not an age.");
+			}
+
+			Console.Read();
+		}
+	}
+}
